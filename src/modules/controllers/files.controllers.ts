5b60e6b1274ed5a03f5216c227ea8150727d6b12
chuprin <chuprin.exceedteam@gmail.com>
2022-05-17 17:10:56 +0300

    Working on mistakes

--- src/modules/controllers/files.controllers.ts
+++ src/modules/controllers/files.controllers.ts
@@ -1,12 +1,11 @@
-import {uid} from 'uid';
-
+import { uid } from 'uid';
+const path = require('path');
 const fs = require('fs');
-const url = 'localhost:4000';
 
 module.exports.getFiles = async (req: any, res: any) => {
     new Promise((resolve) => {
       fs.readdir('public', (err: any, files: any): any => {
-        resolve(files.map((file: any) => `http://${url}/${file}`))
+        resolve(files.map((file: any) => `http://${process.env.url}/${file}`))
       })
     }).then((value: any) => res.send(value))
 }
@@ -14,6 +13,11 @@ module.exports.getFiles = async (req: any, res: any) => {
 module.exports.saveFiles = (req: any, res: any) => {
   const { files } = req;
   Object.values(files).forEach((file: any) => {
-    fs.appendFile(`./public/${uid()}.${file.name}`, file.data, ((err: any) => console.log(err)))
+    fs.appendFile(`./public/${uid()}${path.extname(file.name)}`, file.data, ((err: any) => Error(err)))
   })
+  new Promise((resolve) => {
+    fs.readdir('public', (err: any, files: any): any => {
+      resolve(files.map((file: any) => `http://${process.env.url}/${file}`))
+    })
+  }).then((value: any) => res.send(value))
 }
